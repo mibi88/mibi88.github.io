f8154e69f8f6648c41c8d1c87a7c1920556d88cb
mibi88 <mbcontact50@gmail.com>
2024-11-21 17:36:08 +0100

    Added a dark theme

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 document.onscroll = function() {
     if (window.scrollY > 10) {
-        document.getElementById("bar").style.boxShadow="0pt 4pt darkgray";
-        document.getElementById("up_button").style.display="block";
+        document.getElementById("bar").style.boxShadow =
+                                                "0pt 4pt var(--shadow)";
+        document.getElementById("up_button").style.display = "block";
     }else{
-        document.getElementById("bar").style.boxShadow="0pt 4pt white";
-        document.getElementById("up_button").style.display="none";
+        document.getElementById("bar").style.boxShadow =
+                                                    "0pt 4pt var(--bar-bg)";
+        document.getElementById("up_button").style.display = "none";
     }
 }
 function topFunction() {
