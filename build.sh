8f5db63172d7df126dbe76fe8e809dc02dc7b7ab
mibi88 <mbcontact50@gmail.com>
2025-11-10 23:09:06 +0100

    Cleaned up the HTML generation code

--- build.sh
+++ build.sh
@@ -10,8 +10,6 @@ pdir=pdf
 
 recent_articles_max=5
 
-strip_html_structure=true
-
 mkdir -p $bdir
 
 # Generate the project list
@@ -19,11 +17,54 @@ mkdir -p $bdir
 IFS="
 "
 
+escape_for_html() {
+    # Escape some characters for HTML
+
+    escaped=$1
+
+    escaped=${escaped//&/\&amp;}
+    escaped=${escaped//</\&lt;}
+    escaped=${escaped//>/\&gt;}
+
+    echo "$escaped"
+}
+
+escape_for_sed() {
+    # Escape some characters for sed
+
+    escaped=$1
+
+    escaped=${escaped//\\/\\\\}
+    escaped=${escaped//\//\\/}
+    escaped=${escaped//\&/\\\&}
+
+    echo "$escaped"
+}
+
+strip_styles() {
+    # Strip the styles
+
+    sed -zi "s/<style[^>]*>[^<]*<\/style>//" $1
+    sed -i "s/style=\"[^\"]*\"//" $1
+}
+
+keep_body_only() {
+    # Remove everything except <body>'s contents.
+
+    sed -zi -e "s/.*<body[^>]*>//" -e "s/<\/body>.*//" $1
+}
+
+avoid_tag_replacement() {
+    # FIXME: Do not replace them in <style> tags.
+
+    #sed -i -e "s/{/\&#123;/g" -e "s/}/\&#125;/g" $1
+
+    echo "FIXME: Fix curly brace escaping code."
+}
+
 projects=$(cat $projects)
 
-if [ $strip_html_structure = true ]; then
-    echo "" > $bdir/projects.html
-fi
+echo "" > $bdir/projects.html
 
 for i in ${projects[@]}; do
     name=${i%;*}
@@ -33,38 +74,28 @@ for i in ${projects[@]}; do
 
     preconv $path | groff -mwww -ms -Thtml > $bdir/$path.html
 
-    escaped=$name
-
-    # Escape some characters for HTML
-
-    escaped=${escaped//&/\&amp;}
-    escaped=${escaped//</\&lt;}
-    escaped=${escaped//>/\&gt;}
+    escaped=$(escape_for_html $name)
 
     html=$escaped
 
-    # Escape some characters for sed
-    escaped=${escaped//\\/\\\\}
-    escaped=${escaped//\//\\/}
-    escaped=${escaped//\&/\\\&}
+    escaped=$(escape_for_sed $escaped)
 
     # Postprocess the HTML
-    sed -zi "s/<style[^>]*>[^<]*<\/style>//" $bdir/$path.html
-    sed -i "s/style=\"[^\"]*\"//" $bdir/$path.html
-    sed -zi "s/<title[^>]*>[^<]*<\/title>/<title>$escaped<\/title>\
-<h1>$escaped<\/h1>/" $bdir/$path.html
 
-    if [ $strip_html_structure = true ]; then
-        sed -zi "s/.*<body>//" $bdir/$path.html
-        sed -zi "s/<\/body>.*//" $bdir/$path.html
+    strip_styles $bdir/$path.html
 
-        echo "<h1>$html</h1>$(cat $bdir/$path.html)" > $bdir/$path.html
+    keep_body_only $bdir/$path.html
 
-        sed "/{project}/{
-s/{project}//
-r $bdir/$path.html
-}" project_template.html >> $bdir/projects.html
-    fi
+    # Add the title and put it into the template
+
+    echo "<h1>$html</h1>$(cat $bdir/$path.html)" > $bdir/$path.html
+
+    avoid_tag_replacement $bdir/$path.html
+
+    sed "/{project}/{
+            s/{project}//
+            r $bdir/$path.html
+        }" project_template.html >> $bdir/projects.html
 done
 
 # Generate the recent article list and the article pages
@@ -76,11 +107,14 @@ article_list=""
 
 echo "" > $bdir/recent_articles.html
 
-known_ids=()
-
 mkdir -p $pdir
 
+# The list of already used IDs.
+known_ids=()
+
+# The article count.
 c=0
+
 for i in ${articles[@]}; do
     f=${i#*;}
     id=${i%;*}
@@ -88,9 +122,20 @@ for i in ${articles[@]}; do
     o=$bdir/${f%.*}.html
     p=$pdir/$id.pdf
     ext=${f#*.}
+
+    # Check if the ID has already been used.
+
+    if [[ "$IFS${known_ids[*]}$IFS" =~ "$IFS$id$IFS" ]]; then
+        echo "ID \`$id' already used!"
+        exit 1
+    fi
+
     mkdir -p $(dirname $o)
-    echo $o $ext
+
     keep_styles=false
+
+    # Generate the HTML code.
+
     case "$ext" in
         lyx) lyx --export-to xhtml $o $f
              keep_styles=true ;;
@@ -99,6 +144,8 @@ for i in ${articles[@]}; do
         html) cp $f $o ;;
         *) echo "Unsupported file type. Skipping..."
     esac
+
+    # Generate the PDFs.
 
     case "$ext" in
         lyx) lyx --export-to pdf $p $f
@@ -109,11 +156,13 @@ for i in ${articles[@]}; do
         *) echo "PDF export not supported for this file type..."
     esac
 
+    # Strip some HTML code
+
     title=$(sed -z -e "s/.*<h1[^>]*>//g" -e "s/<\/h1>.*//g" $o)
 
-    article_list+="<a href=\"$adir/$id.html\">$title</a></br>"
+    # Add the article to the article list
 
-    c=$(($c+1))
+    article_list+="<a href=\"$adir/$id.html\">$title</a></br>"
 
     # Strip all the unneeded stuff
 
@@ -122,16 +171,14 @@ for i in ${articles[@]}; do
                 -e "s/<\/style>.*<body[^>]*>/<\/style>/" \
                 -e "s/<\/body>.*//" $o
     else
-        sed -zi -e "s/.*<body[^>]*>//" -e "s/<\/body>.*//" $o
+        keep_body_only $o
     fi
 
     echo "<div class=\"article\">$(cat $o)</div>" > $o
 
-    # Escape some characters for sed
-    escaped=$title
-    escaped=${escaped//\\/\\\\}
-    escaped=${escaped//\//\\/}
-    escaped=${escaped//\&/\\\&}
+    escaped=$(escape_for_sed $title)
+
+    avoid_tag_replacement $o
 
     sed -e "s/{title}/$escaped/" \
         -e "/{article}/{
@@ -140,14 +187,16 @@ for i in ${articles[@]}; do
             }" \
         article_template.html > $adir/$id.html
 
-    if [ $c -le $recent_articles_max ]; then
+    # Add the article to the recent articles if we haven't added at most
+    # $recent_articles_max articles to it.
+
+    if [ $c -lt $recent_articles_max ]; then
         cat $o >> $bdir/recent_articles.html
     fi
 
-    if [[ "$IFS${known_ids[*]}$IFS" =~ "$IFS$id$IFS" ]]; then
-        echo "ID \`$id' already used!"
-        exit 1
-    fi
+    # Increase the article count
+
+    c=$(($c+1))
 
     known_ids+=($id)
 done
@@ -158,34 +207,31 @@ preconv welcome.ms | groff -mwww -ms -Thtml > $bdir/welcome.html
 sed -zi -e "s/.*<body[^>]*>//" -e "s/<\/body>.*//" -e "s/align=\"center\"//g" \
         -e "s/style=\"[^\"]*\"//" $bdir/welcome.html
 
-echo $article_list
-
 # Do not generate index.html if $strip_html_structure is disabled, as it would
 # produce invalid html.
 
 IFS=""
 
-# Escape some characters of the article list for sed
-article_list=${article_list//\\/\\\\}
-article_list=${article_list//\//\\/}
-article_list=${article_list//\&/\\\&}
+article_list=$(escape_for_sed $article_list)
 
-if [ $strip_html_structure = true ]; then
-    sed -e "s/{current_year}/$(date +%Y)/g" \
-        -e "s/{article_list}/$article_list/" \
-        -e "/{welcome}/{
-                s/{welcome}//
-                r $bdir/welcome.html
-            }" \
-        -e "/{projects}/{
-                s/{projects}//
-                r $bdir/projects.html
-            }" \
-        -e "/{articles}/{
-                s/{articles}//
-                r $bdir/recent_articles.html
-            }" \
-        index_template.html > index.html
-fi
+avoid_tag_replacement $bdir/welcome.html
+avoid_tag_replacement $bdir/projects.html
+avoid_tag_replacement $bdir/recent_articles.html
+
+sed -e "s/{current_year}/$(date +%Y)/g" \
+    -e "s/{article_list}/$article_list/" \
+    -e "/{welcome}/{
+            s/{welcome}//
+            r $bdir/welcome.html
+        }" \
+    -e "/{projects}/{
+            s/{projects}//
+            r $bdir/projects.html
+        }" \
+    -e "/{articles}/{
+            s/{articles}//
+            r $bdir/recent_articles.html
+        }" \
+    index_template.html > index.html
 
 unset IFS
